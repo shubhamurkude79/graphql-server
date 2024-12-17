421f254d78d8ea9ead0ab6fde295eae66f8c9a1a
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-12-18 01:39:36 +0530

    category and brand properties and its required data was added

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const typeDefs = gql`
         id: ID!
         name: String!
         description: String
+        category: String!
+        brand: String!
         price: Float!
         imageUrl: String
     }
@@ -24,38 +26,55 @@ const typeDefs = gql`
 // List of products as mock data
 const productsList = [
     { id: '1', name: 'Airpods', description: 'A great product',
+        category: 'Electronics', brand: 'Apple', 
         price: 29.99, imageUrl: '/images/electronics/large/airpods-big.jpg' },
       { id: '2', name: 'Balck Smartwatch', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Samsung',
         price: 49.99, imageUrl: '/images/electronics/large/black-smartwatch-big.jpg' },
       { id: '3', name: 'Camer', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Canon',
         price: 49.99, imageUrl: '/images/electronics/large/camera-big.jpg' },
       { id: '4', name: 'Digital Camera', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Canon',
         price: 49.99, imageUrl: '/images/electronics/large/digital-camera-big.jpeg' },
       { id: '5', name: 'Earpod', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Samsung',
         price: 49.99, imageUrl: '/images/electronics/large/earpods-big.jpg' },
       { id: '6', name: 'Headset', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Sony',
         price: 49.99, imageUrl: '/images/electronics/large/wired-headset-big.jpg' },
       { id: '7', name: 'Ipad Air', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Apple',
         price: 49.99, imageUrl: '/images/electronics/large/ipad-air-big.webp' },
       { id: '8', name: 'Ipad', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Apple',
         price: 49.99, imageUrl: '/images/electronics/large/ipad-big.png' },
       { id: '9', name: 'Iphone Earbuds', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Apple',
         price: 49.99, imageUrl: '/images/electronics/large/iphone-earbuds-big.jpg' },
       { id: '10', name: 'Macbook', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Apple',
         price: 49.99, imageUrl: '/images/electronics/large/macbook-big.jpeg' },
       { id: '11', name: 'Mouse', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Zebronics',
         price: 49.99, imageUrl: '/images/electronics/large/mouse-big.jpeg' },
       { id: '12', name: 'Old Laptop', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Asus',
         price: 49.99, imageUrl: '/images/electronics/large/old-laptop-big.webp' },
       { id: '13', name: 'Pendrive', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Sony',
         price: 49.99, imageUrl: '/images/electronics/large/pendrive-big.webp' },
       { id: '14', name: 'Printer', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Samsung',
         price: 49.99, imageUrl: '/images/electronics/large/printer-big.webp' },
       { id: '15', name: 'Smartwatch', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Samsung',
         price: 49.99, imageUrl: '/images/electronics/large/smartwatch-big.png' },
       { id: '16', name: 'Tablet', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Samsung',
         price: 49.99, imageUrl: '/images/electronics/large/tablet-big.jpg' },
       { id: '17', name: 'Wireless Headset', description: 'Another amazing product',
+        category: 'Electronics', brand: 'Sony',
         price: 49.99, imageUrl: '/images/electronics/large/wireless-headset-big.jpg' },
   ];
 
