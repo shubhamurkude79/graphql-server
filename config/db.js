14258eb1433e4f7afcaaea0adfcfb8305839bf93
shubhamurkude79 <shubhamurkude79@gmail.com>
2025-02-04 01:44:14 +0530

    folder restructuring done

--- config/db.js
+++ config/db.js
@@ -3,10 +3,13 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URI, { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        });
         console.log('✅ MongoDB Connected');
     } catch (error) {
-        console.error('❌ MongoDB Connection Error:', error);
+        console.error(`❌ MongoDB Connection Error: ${error.message}`);
         process.exit(1);
     }
 };
